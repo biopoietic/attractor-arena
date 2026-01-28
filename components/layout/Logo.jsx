2ada9f170c67e9ef5e723f544c2d6b9458f7e9c6
Shane Vincent <theshanergy@gmail.com>
2026-01-28 12:06:00 -0800

    homepage link logo

--- components/layout/Logo.jsx
+++ components/layout/Logo.jsx
@@ -1,11 +1,12 @@
 import { Activity } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
 	return (
-		<div className='flex items-center gap-3 font-sans'>
+		<Link to='/' className='flex items-center gap-3 font-sans hover:opacity-80 transition-opacity'>
 			<Activity size={18} className='text-brand-highlight animate-pulse' />
 			<h1 className='text-lg font-bold tracking-tighter uppercase'>Attractor Arena</h1>
-		</div>
+		</Link>
 	)
 }
 
