51dac917870cf92306d2b5932049f425ccb94ff5
Shane Vincent <theshanergy@gmail.com>
2026-07-20 12:05:11 -0700

    Migrate analytics tracking to Umami

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,16 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Script
+				src='https://analytics.digitalnature.io/script.js'
+				data-website-id='79b71caf-6f4d-4749-8ae5-7f07e1fd849b'
+				data-domains='attractor-arena.biopoietic.com'
+				strategy='afterInteractive'
+			/>
+			<Component {...pageProps} />
+		</>
+	)
 }
